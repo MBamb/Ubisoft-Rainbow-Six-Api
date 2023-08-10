c35c41c66fd18c7d1c26d9771fdb0ad006a557c0
Max Böhm <thelaxito@hotmail.de>
2023-08-10 23:46:45 +0200

    Added season stats model

---
+++ src/main/java/de/vault/models/SeasonStats.java
@@ -0,0 +1,24 @@
+package de.vault.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SeasonStats {
+    private String id;
+    private long maxRank;
+    private long maxRankPoints;
+    private long rank;
+    private long rankPoints;
+    private long seasonId;
+    private long topRankPosition;
+    private long deaths;
+    private long kills;
+    private long matchesWon;
+    private long matchesLost;
+    private long matchesAbandoned;
+}
---
+++ src/main/java/de/vault/models/SeasonStatsType.java
@@ -0,0 +1,5 @@
+package de.vault.models;
+
+public enum SeasonStatsType {
+    RANKED, CASUAL, EVENT, WARMUP
+}
